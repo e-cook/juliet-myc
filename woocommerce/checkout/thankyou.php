bc7ebe8cf9ef3b73f34b5046a1910d640945b4a1
julian-upc <julian.pfeifle@upc.edu>
2017-08-08 06:14:41 +0200

    print delivery date on thankyou

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -17,29 +17,29 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+    exit;
 }
 ?>
 
 <div class="woocommerce-order">
 
-	<?php if ( $order ) : ?>
+    <?php if ( $order ) : ?>
 
-		<?php if ( $order->has_status( 'failed' ) ) : ?>
+	<?php if ( $order->has_status( 'failed' ) ) : ?>
 
-			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed"><?php _e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'juliet' ); ?></p>
+	    <p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed"><?php _e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'juliet' ); ?></p>
 
-			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions">
-				<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php _e( 'Pay', 'juliet' ) ?></a>
-				<?php if ( is_user_logged_in() ) : ?>
-					<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" class="button pay"><?php _e( 'My account', 'juliet' ); ?></a>
-				<?php endif; ?>
-			</p>
+	    <p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions">
+		<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php _e( 'Pay', 'juliet' ) ?></a>
+		<?php if ( is_user_logged_in() ) : ?>
+		    <a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" class="button pay"><?php _e( 'My account', 'juliet' ); ?></a>
+		<?php endif; ?>
+	    </p>
 
-		<?php else : ?>
+	<?php else : ?>
 
             <div class="thankyou-message">
-			    <p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( '<h2>Thank you!</h2> <p>Your order has been received.</p>', 'juliet' ), $order ); ?></p>
+		<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( '<h2>Thank you!</h2> <p>Your order has been received.</p>', 'juliet' ), $order ); ?></p>
             </div>
 
             <div class="order-details">
@@ -55,6 +55,11 @@ if ( ! defined( 'ABSPATH' ) ) {
                         <strong><?php echo wc_format_datetime( $order->get_date_created() ); ?></strong>
                     </li>
 
+		    <li>
+			- <?php _e( 'Delivery date:', 'juliet' ); ?>
+			<strong><?php echo date('l, F j Y', strtotime( get_post_meta( $order->get_id(), '_delivery_date', true ) ) ); ?></strong>
+		    </li>
+
                     <li>
                         - <?php _e( 'Total:', 'juliet' ); ?>
                         <strong><?php echo $order->get_formatted_order_total(); ?></strong>
@@ -62,17 +67,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 
                     <?php if ( $order->get_payment_method_title() ) : ?>
 
-                    <li>
-                        - <?php _e( 'Payment method:', 'juliet' ); ?>
-                        <strong><?php echo wp_kses_post( $order->get_payment_method_title() ); ?></strong>
-                    </li>
+			<li>
+                            - <?php _e( 'Payment method:', 'juliet' ); ?>
+                            <strong><?php echo wp_kses_post( $order->get_payment_method_title() ); ?></strong>
+			</li>
 
                     <?php endif; ?>
 
                 </ul>
             </div>
 
-		<?php endif; ?>
+	<?php endif; ?>
 
         <br>
 
@@ -81,10 +86,10 @@ if ( ! defined( 'ABSPATH' ) ) {
             <?php do_action( 'woocommerce_thankyou', $order->get_id() ); ?>
         </div>
 
-	<?php else : ?>
+    <?php else : ?>
 
-		<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( '<h2>Thank you.</h2> <p>Your order has been received.</p>', 'juliet' ), null ); ?></p>
+	<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( '<h2>Thank you.</h2> <p>Your order has been received.</p>', 'juliet' ), null ); ?></p>
 
-	<?php endif; ?>
+    <?php endif; ?>
 
 </div>
