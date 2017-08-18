f59c32d5db009f06ab6119ac2f65e1a3ada1e7ae
e-cook <e-cook@>
2017-08-18 23:45:38 +0200

    allow html in midsection

--- parts/frontpage-midsection.php
+++ parts/frontpage-midsection.php
@@ -14,7 +14,7 @@ if ( $juliet_frontpage_midsection_show == 1 ) { ?>
     <div class="frontpage-midsection">
         <div class="row" data-fluid=".entry-summary,.entry-title">
             <div class="col-sm-6">
-                <?php echo esc_html( juliet_get_option( 'juliet_frontpage_midsection_text' ) ); ?>
+                <?php echo juliet_get_option( 'juliet_frontpage_midsection_text' ); ?>
             </div>
             <div class="col-sm-6">
                 <?php echo '<img class="img-responsive" id="juliet_frontpage_midsection_picture" src=' . esc_html( juliet_get_option( 'juliet_frontpage_midsection_picture' ) ) . '>'; ?>
