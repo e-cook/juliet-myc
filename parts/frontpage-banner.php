21cae7b93a2c6ed8d0f954d42c975ea9e118a4db
e-cook <e-cook@>
2017-12-09 19:58:25 +0100

    image carousel on front page

--- parts/frontpage-banner.php
+++ parts/frontpage-banner.php
@@ -10,18 +10,38 @@
 
 /*** Banner ***/ 
 
-$header_image = get_header_image(); 
+$header_images = get_uploaded_header_images(); 
+error_log("header_images: " . var_export($header_images,1));
+
 $juliet_banner_heading = juliet_get_option('juliet_banner_heading');
 $juliet_banner_description = juliet_get_option('juliet_banner_description');
 $juliet_banner_url = juliet_get_option('juliet_banner_url');
-if($header_image != '') { 
+if ( sizeof($header_images) > 0 ) { 
 ?>
 <!-- Frontpage Banner -->
 <div class="container">
     <div class="frontpage-banner">
-        <img src="<?php echo esc_url($header_image) ?>" alt="<?php echo esc_attr($juliet_banner_heading); ?>" class="img-responsive" />
-		
-		<?php if($juliet_banner_heading != '' || $juliet_banner_description != '') { ?>
+	<div id="myc-frontpage-carousel" class="carousel slide" data-ride="carousel">
+	    <!-- Indicators -->
+	    <ol class="carousel-indicators">
+	    <?php for ($i=0; $i < sizeof( $header_images ); $i++) { ?>
+		<li data-target="#myc-frontpage-carousel" data-slide-to="<?php echo $i; ?>"<?php if ( 0 == $i ) { echo ' class="active"'; } ?>></li>
+	    <?php } ?>
+	    </ol>
+
+	    <!-- Wrappers for slides -->
+	    <div class="carousel-inner">
+		<?php
+		$first = 1;
+		foreach ( $header_images as $id => $image ) { ?>
+		    <div class="item<?php if ( 1 == $first ) { $first = 0; echo ' active'; } ?>">
+			<img src="<?php echo esc_url($image['url']) ?>" alt="<?php echo esc_attr($juliet_banner_heading); ?>" class="img-responsive" />
+		    </div>
+		<?php } ?>
+	    </div>
+	</div>
+	
+	<?php if($juliet_banner_heading != '' || $juliet_banner_description != '') { ?>
         <div class="caption">
             
             <?php if($juliet_banner_url != '' && $juliet_banner_heading != '') { ?><h2><a href="<?php echo esc_url($juliet_banner_url); ?>"><?php echo esc_html($juliet_banner_heading); ?></a></h2><?php } ?>
